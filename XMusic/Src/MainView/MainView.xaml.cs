c10d48a3b62db743bb31e60f1b5d28d201f43aad
Xioa <132796255@qq.com>
2024-12-23 09:21:30 +0800

    UI

---
+++ XMusic/Src/MainView/MainView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace XMusic.Src.MainView;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ XMusic/Src/MainView/ViewModel/MainViewModel.cs
@@ -0,0 +1,7 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace XMusic.Src.MainView.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+}
